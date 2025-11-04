be6826b5b75e22435cec23756c3696d172d7d08d
Sean Pearce <14220530+smpita@users.noreply.github.com>
2025-11-04 12:20:17 -0800

    add migrations

---
+++ database/migrations/2025_11_04_191432_create_recipes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->index(); // This should probably be unique, but wasn't in the spec
+            $table->text('description')->nullable();
+            $table->string('author_email')->index();
+            $table->string('slug')->unique();
+            $table->foreignId('ingredients_id')->constrained('ingredients');
+            $table->json('steps');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recipes');
+    }
+};
